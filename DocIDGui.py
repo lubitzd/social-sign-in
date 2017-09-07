3cd65a95dc9c863bce6e1bba3e927035eae473b1
Dan Lubitz <lubitzd@gmail.com>
2017-09-07 12:55:37 -0400

    Converted to Class format

--- DocIDGui.py
+++ DocIDGui.py
@@ -1,51 +1,68 @@
 import Tkinter as tk
 
-class DocIDGui(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid()
+
+class SocialSignInApp(tk.Tk):
+    
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (DocIDPage): # and IdlePage, RegPage
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("DocIDPage")
+
+
+    def show_frame(self, page_name):
+        # Show a frame for the given page name
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class DocIDPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
         self.createDocIDWidgets()
-        self.createRegWidgets()
-
-        self.switchFrames(self.docIDFrame)
-
-
-    def switchFrames(self, frame):
-        if frame == 'docID':
-            self.docIDFrame.tkraise()
-        elif frame == 'reg':
-            self.regFrame.tkraise()
-
 
     def createDocIDWidgets(self):
-        self.docIDFrame = tk.Frame(self)
-        self.docIDFrame.grid(row=0, column=0)
+        instructionsLabel = tk.Label(self, text='[How to get Doc IDs]')
+        instructionsLabel.grid(row=0, column=0, columnspan=2)
 
-        self.instructionsLabel = tk.Label(self.docIDFrame, text='[How to get Doc IDs]')
-        self.instructionsLabel.grid(row=0, column=0, columnspan=2)
+        masterLabel = tk.Label(self, text='Master ID:')
+        masterLabel.grid(row=1, column=0)
+        danceLabel = tk.Label(self, text='Dance ID:')
+        danceLabel.grid(row=2, column=0)
+        earlyLabel = tk.Label(self, text='Early ID:')
+        earlyLabel.grid(row=3, column=0)
 
-        self.masterLabel = tk.Label(self.docIDFrame, text='Master ID:')
-        self.masterLabel.grid(row=1, column=0)
-        self.danceLabel = tk.Label(self.docIDFrame, text='Dance ID:')
-        self.danceLabel.grid(row=2, column=0)
-        self.earlyLabel = tk.Label(self.docIDFrame, text='Early ID:')
-        self.earlyLabel.grid(row=3, column=0)
-
-        self.masterEntry = tk.Entry(self.docIDFrame, width=45)
-        self.masterEntry.grid(row=1, column=1)
-        self.danceEntry = tk.Entry(self.docIDFrame, width=45)
-        self.danceEntry.grid(row=2, column=1)
-        self.earlyEntry = tk.Entry(self.docIDFrame, width=45)
-        self.earlyEntry.grid(row=3, column=1)
+        masterEntry = tk.Entry(self, width=45)
+        masterEntry.grid(row=1, column=1)
+        danceEntry = tk.Entry(self, width=45)
+        danceEntry.grid(row=2, column=1)
+        earlyEntry = tk.Entry(self, width=45)
+        earlyEntry.grid(row=3, column=1)
         
-        self.goButton = tk.Button(self.docIDFrame, text='Go')
-        self.goButton.grid(row=4, column=0, columnspan=2)
+        goButton = tk.Button(self, text='Go') #, command=lambda: controller.show_frame("IdlePage"))
+        goButton.grid(row=4, column=0, columnspan=2)
 
 
-    def createRegWidgets(self):
-        self.RegFrame = tk.Frame(self)
-        self.RegFrame.grid(row=0, column=0)
 
-        self.nameEntry = tk.Entry(self.RegFrame)
-        self.nameEntry.grid(row=0, column=0)
+#        nameEntry = tk.Entry(self.RegFrame)
+#        nameEntry.grid(row=0, column=0)
         
