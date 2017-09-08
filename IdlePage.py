7d318cd13d185989b86afa887945b7d6c254b3d8
Dan Lubitz <lubitzd@gmail.com>
2017-09-08 15:13:05 -0400

    Removed commented code

--- IdlePage.py
+++ IdlePage.py
@@ -15,19 +15,18 @@ class IdlePage(tk.Frame):
         self.rowconfigure(3, weight=1)
         self.rowconfigure(4, weight=1)
         self.columnconfigure(0, weight=1)
-        #self.columnconfigure(1, weight=1)
         self.columnconfigure(2, weight=1)
 
         scanLabel = tk.Label(self, text="Scan a card", font=tkFont.Font(size=14))
         scanLabel.grid(row=0, column=1)
 
-        or1Label = tk.Label(self, text="or")#, font=tkFont.Font(size=14))
+        or1Label = tk.Label(self, text="or")
         or1Label.grid(row=1, column=1, pady=5)
 
         newButton = tk.Button(self, text="New Account", command=lambda: self.controller.show_frame("RegPage"))
         newButton.grid(row=2, column=1, ipady=5, ipadx=10)
 
-        or2Label = tk.Label(self, text="or")#, font=tkFont.Font(size=14))
+        or2Label = tk.Label(self, text="or")
         or2Label.grid(row=3, column=1, pady=5)
 
         nameLabel = tk.Label(self, text="Name:")
