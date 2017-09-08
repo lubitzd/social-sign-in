470b1d71c684fc988fbc6a892230c8f976f2a370
Dan Lubitz <lubitzd@gmail.com>
2017-09-08 12:15:36 -0400

    Added variables to fields, tweaked GUI

--- SocialSignInApp.py
+++ SocialSignInApp.py
@@ -49,7 +49,7 @@ class DocIDPage(tk.Frame):
         self.rowconfigure(4, pad=5)
         self.columnconfigure(0, pad=5)
         
-        instructionsLabel = tk.Label(self, text="[How to get Doc IDs]")
+        instructionsLabel = tk.Label(self, text="To get the doc IDs, open the doc in your browser.\nThe part of the URL after the \"/d/\" and up to the next \"/\" is the ID.")
         instructionsLabel.grid(row=0, column=0, columnspan=2)
 
         masterLabel = tk.Label(self, text="Master ID:")
@@ -59,11 +59,17 @@ class DocIDPage(tk.Frame):
         earlyLabel = tk.Label(self, text="Early ID:")
         earlyLabel.grid(row=3, column=0)
 
-        masterEntry = tk.Entry(self, width=45)
+        # Master Doc ID
+        self.masterDocIDVar = tk.StringVar()
+        masterEntry = tk.Entry(self, width=45, textvariable=self.masterDocIDVar)
         masterEntry.grid(row=1, column=1, padx=5)
-        danceEntry = tk.Entry(self, width=45)
+        # Dance Doc ID
+        self.danceDocIDVar = tk.StringVar()
+        danceEntry = tk.Entry(self, width=45, textvariable=self.danceDocIDVar)
         danceEntry.grid(row=2, column=1, padx=5)
-        earlyEntry = tk.Entry(self, width=45)
+        # Early Doc ID
+        self.earlyDocIDVar = tk.StringVar()
+        earlyEntry = tk.Entry(self, width=45, textvariable=self.earlyDocIDVar)
         earlyEntry.grid(row=3, column=1, padx=5)
         
         goButton = tk.Button(self, text="Go", command=lambda: self.controller.show_frame("IdlePage"))
@@ -95,34 +101,60 @@ class RegPage(tk.Frame):
         self.createRegWidgets()
 
     def createRegWidgets(self):
-        nameEntry = tk.Entry(self)
-        nameEntry.grid(row=0, column=0, padx=5, sticky="sw")
-        rcsEntry = tk.Entry(self)
-        rcsEntry.grid(row=1, column=0, padx=5, sticky="nw")
+        nameLabel = tk.Label(self, text="Name:")
+        nameLabel.grid(row=0, column=0, sticky="se")
+        # Name
+        self.nameVar = tk.StringVar()
+        nameEntry = tk.Entry(self, textvariable=self.nameVar)
+        nameEntry.grid(row=0, column=1, columnspan=2, padx=5, sticky="sw")
+
+        rcsLabel = tk.Label(self, text="RCS ID/Email:")
+        rcsLabel.grid(row=1, column=0, sticky="ne")
+        # RCS (or email)
+        self.rcsVar = tk.StringVar()
+        rcsEntry = tk.Entry(self, textvariable=self.rcsVar)
+        rcsEntry.grid(row=1, column=1, columnspan=2, padx=5, sticky="nw")
 
         rfidLabel = tk.Label(self, text="RFID #:")
-        rfidLabel.grid(row=2, column=0, padx=5, sticky="w")
+        rfidLabel.grid(row=2, column=0, padx=5, sticky="e")
+        # RFID Num
+        self.rfidNumVar = tk.StringVar()
+        rfidNumLabel = tk.Label(self, textvariable=self.rfidNumVar)
+        rfidNumLabel.grid(row=2, column=1, sticky="w")
 
         membershipFrame = tk.Frame(self, borderwidth=1, relief=tk.GROOVE)
-        membershipFrame.grid(row=0, rowspan=2, column=1, columnspan=2, pady=5)
-        memberCheckbutton = tk.Checkbutton(membershipFrame, text="Member of Club")
+        membershipFrame.grid(row=0, rowspan=2, column=3, columnspan=2, pady=5)
+        # Club Member
+        self.clubMemberVar = tk.StringVar()
+        memberCheckbutton = tk.Checkbutton(membershipFrame, text="Member of Club", \
+                            variable=self.clubMemberVar, onvalue="member", offvalue="")
         memberCheckbutton.grid()
+        # Member Type
         self.memberTypeVar = tk.StringVar()
         memberType = tk.OptionMenu(membershipFrame, self.memberTypeVar, "Student", "Community", "Former")
         memberType.grid()
 
-        earlyCheckbutton = tk.Checkbutton(self, text="Signed Up Early")
-        earlyCheckbutton.grid(row=2, column=1, columnspan=2, sticky="w")
+        # Early
+        self.earlyVar = tk.IntVar()
+        earlyCheckbutton = tk.Checkbutton(self, text="Signed Up Early", variable=self.earlyVar)
+        earlyCheckbutton.grid(row=2, column=3, columnspan=2, sticky="w")
 
-        emailCheckbutton = tk.Checkbutton(self, text="On Email List")
-        emailCheckbutton.grid(row=3, column=1, columnspan=2, sticky="w")
+        # Email
+        self.emailVar = tk.IntVar()
+        emailCheckbutton = tk.Checkbutton(self, text="On Email List", variable=self.emailVar)
+        emailCheckbutton.grid(row=3, column=3, columnspan=2, sticky="w")
 
-        amountDueLabel = tk.Label(self, text="Amount Due: $3", font=tkFont.Font(size=12))
-        amountDueLabel.grid(row=4, column=0)
+        amountDueLabel = tk.Label(self, text="Amount Due: $", font=tkFont.Font(size=12))
+        amountDueLabel.grid(row=4, column=1)
+        # Amount Due Num
+        self.amountDueNumVar = tk.IntVar()
+        amountDueNumLabel = tk.Label(self, textvariable=self.amountDueNumVar, font=tkFont.Font(size=12))
+        amountDueNumLabel.grid(row=4, column=2, sticky="w")
 
-        cancelButton = tk.Button(self, text="Cancel", background="#EB5757")
-        cancelButton.grid(row=4, column=1, pady=10, sticky="se")
+        cancelButton = tk.Button(self, text="Cancel", background="#EB5757", \
+                        command=lambda: self.controller.show_frame("IdlePage"))
+        cancelButton.grid(row=4, column=3, pady=10, sticky="se")
         submitButton = tk.Button(self, text="Submit", background="#6FCF97")
-        submitButton.grid(row=4, column=2, pady=10, sticky="se")
+        submitButton.grid(row=4, column=4, pady=10, sticky="se")
 
         
