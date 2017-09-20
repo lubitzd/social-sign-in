ab6ba7d03e05c4e677e1ab120dfd0747d2122749
Keith Fieldhouse <lubitzd@gmail.com>
2017-09-20 18:30:18 -0400

    Made gui scaling configurable

--- RegPage.py
+++ RegPage.py
@@ -9,27 +9,27 @@ class RegPage(tk.Frame):
         self.createRegWidgets()
 
     def createRegWidgets(self):
-        nameLabel = tk.Label(self, text="Name:")
+        nameLabel = tk.Label(self, text="Name:", font=self.controller.customFont)
         nameLabel.grid(row=0, column=0, sticky="se")
         # Name
         self.nameVar = tk.StringVar()
-        nameEntry = tk.Entry(self, textvariable=self.nameVar)
+        nameEntry = tk.Entry(self, textvariable=self.nameVar, font=self.controller.customFont)
         self.nameVar.trace("w", self.nameUpdatedCallback)
         nameEntry.grid(row=0, column=1, columnspan=2, padx=5, sticky="sw")
 
-        rcsLabel = tk.Label(self, text="RCS ID/Email:")
+        rcsLabel = tk.Label(self, text="RCS ID/Email:", font=self.controller.customFont)
         rcsLabel.grid(row=1, column=0, sticky="ne")
         # RCS (or email)
         self.rcsVar = tk.StringVar()
-        rcsEntry = tk.Entry(self, textvariable=self.rcsVar)
+        rcsEntry = tk.Entry(self, textvariable=self.rcsVar, font=self.controller.customFont)
         self.rcsVar.trace("w", self.rcsUpdatedCallback)
         rcsEntry.grid(row=1, column=1, columnspan=2, padx=5, sticky="nw")
 
-        rfidLabel = tk.Label(self, text="RFID #:")
+        rfidLabel = tk.Label(self, text="RFID #:", font=self.controller.customFont)
         rfidLabel.grid(row=2, column=0, padx=5, sticky="e")
         # RFID Num
         self.rfidNumVar = tk.StringVar()
-        rfidNumLabel = tk.Label(self, textvariable=self.rfidNumVar)
+        rfidNumLabel = tk.Label(self, textvariable=self.rfidNumVar, font=self.controller.customFont)
         rfidNumLabel.grid(row=2, column=1, sticky="w")
 
         membershipFrame = tk.Frame(self, borderwidth=1, relief=tk.GROOVE)
@@ -37,38 +37,42 @@ class RegPage(tk.Frame):
         # Club Member
         self.clubMemberVar = tk.IntVar()
         self.memberCheckbutton = tk.Checkbutton(membershipFrame, text="Member of Club", \
-                            variable=self.clubMemberVar, command=self.memberBoxCallback)
+                            variable=self.clubMemberVar, command=self.memberBoxCallback, \
+                            font=self.controller.customFont)
         self.memberCheckbutton.grid()
         # Member Type
         self.memberTypeVar = tk.StringVar()
         options = ["Student", "Community", "Former"]
         memberType = tk.OptionMenu(membershipFrame, self.memberTypeVar, \
                                    *options, command=self.memberTypeCallback)
+        memberType['menu'].config(font=self.controller.customFont)
         memberType.grid()
 
         # Early
         self.earlyVar = tk.IntVar()
         earlyCheckbutton = tk.Checkbutton(self, text="Signed Up Early", \
-                                        variable=self.earlyVar, command=self.earlyBoxCallback)
+                                        variable=self.earlyVar, command=self.earlyBoxCallback, \
+                                        font=self.controller.customFont)
         earlyCheckbutton.grid(row=2, column=3, columnspan=2, sticky="w")
 
         # Email
         self.emailVar = tk.IntVar()
-        emailCheckbutton = tk.Checkbutton(self, text="On Email List", variable=self.emailVar)
+        emailCheckbutton = tk.Checkbutton(self, text="On Email List", variable=self.emailVar, \
+                                        font=self.controller.customFont)
         emailCheckbutton.grid(row=3, column=3, columnspan=2, sticky="w")
 
-        amountDueLabel = tk.Label(self, text="Amount Due: $", font=tkFont.Font(size=12))
+        amountDueLabel = tk.Label(self, text="Amount Due: $", font=self.controller.customFontLarge)
         amountDueLabel.grid(row=4, column=1)
         # Amount Due Num
         self.amountDueNumVar = tk.IntVar()
-        amountDueNumLabel = tk.Label(self, textvariable=self.amountDueNumVar, font=tkFont.Font(size=12))
+        amountDueNumLabel = tk.Label(self, textvariable=self.amountDueNumVar, font=self.controller.customFontLarge)
         amountDueNumLabel.grid(row=4, column=2, sticky="w")
 
         cancelButton = tk.Button(self, text="Cancel", background="#EB5757", \
-                        command=self.cancelButtonCallback)
+                        command=self.cancelButtonCallback, font=self.controller.customFont)
         cancelButton.grid(row=4, column=3, pady=10, sticky="se")
         submitButton = tk.Button(self, text="Submit", background="#6FCF97", \
-                        command=self.submitButtonCallback)
+                        command=self.submitButtonCallback, font=self.controller.customFont)
         submitButton.grid(row=4, column=4, pady=10, sticky="se")
 
 
